66f6edc4a930e34550fd13c748f531699b194038
Gonçalo Nuno Bernardo <up201606058@fe.up.pt>
2018-10-21 19:30:02 +0100

    ay socÓRROÓÓ´+O+OÓÓÓ

--- t1/MyRectangle.js
+++ t1/MyRectangle.js
@@ -3,11 +3,9 @@
  * @param gl {WebGLRenderingContext}
  * @constructor
  */
-
- 
+	
 class MyRectangle extends CGFobject {
-
-	/* Assuming:
+		/* Assuming:
 		x1,y1
 		  *_________________*
 		  |					|
@@ -15,58 +13,64 @@ class MyRectangle extends CGFobject {
 		  *_________________* 
 		  				  x2,y2
 	*/
+    constructor(scene, x1, y1, x2, y2) {
+        super(scene);
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
 
-	constructor(scene, x1, y1, x2, y2) {
-		super(scene);
-		this.x1 = x1;
-		this.x2 = x2;
-		this.y1 = y1;
-		this.y2 = y2;
+        this.initBuffers();
+    };
 
-		this.lengthX = Math.abs(x2-x1);
-		this.lengthY = Math.abs(y2-y1);
+    initBuffers() {
+        this.vertices = [
+            this.x1, this.y1, 0,
+            this.x2, this.y1, 0,
+            this.x1, this.y2, 0,
+            this.x2, this.y2, 0,
+        ];
 
-		this.initBuffers();
-	};
+        // defines the normals
+        this.normals = [
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,
+            0, 0, 1,
+        ];
 
-	initBuffers() {
+        // defines the indices
+        this.indices = [
+            0, 1, 2,
+            3, 2, 1,
+        ];
 
-		this.vertices = [
-			this.x1,this.y1,0,
-			this.x2,this.y1,0,
-			this.x1,this.y2,0,
-			this.x2,this.y2,0
-		];
+        // defines the texture coordinates
+        this.texCoords = [
+            0, 0,
+            (this.x2 - this.x1), 0,
+            0, (this.y1 - this.y2),
+            (this.x2 - this.x1), (this.y1 - this.y2),
 
-		this.indices = [
-			0, 2, 1,
-			3, 1, 2
-		];
+        ];
 
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    };
 
-		this.normals = [
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1
-		];
+    setSAndT(s, t) {
 
-		this.textureSet = false;
+        let c = this.x2 - this.x1;
+        var h = this.y2 - this.y1;
 
-		this.primitiveType = this.scene.gl.TRIANGLES;
-		this.initGLBuffers();
-	};
+        this.texCoords = [
+            0, h / t,
+            c / s, h / t,
+            0, 0,
+            c / s, 0,
+        ];
 
-	setTexCoords(lengthS, lengthT){
-		this.texCoords = [
-			0, 0,
-			this.lengthX/lengthS, 0,
-			0, this.lengthY/lengthT,
-			this.lengthX/lengthS, this.lengthY/lengthT
-		];
+        this.updateTexCoordsGLBuffers();
+    }
 
-		this.textureSet = true;
-
-		this.updateTexCoordsGLBuffers();
-	}
 };
